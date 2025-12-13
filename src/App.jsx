79c9f4615c32d779b84e697d3b7b937000e6086b
Gizem Yıldız Allıkan <gzmallkn@gmail.com>
2025-12-13 12:18:00 +0300

    not ready yet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import converterForm from "./components/converterForm";
+const App = () => {
+  return (
+    <div className="currency-conventer">
+      <h2 className="conventer-title">Currency Converter</h2>
+     <ConverterForm/>
+    </div>
+  );
+};
+
+export default App;
